8aff051ea8cbd7e17097a501e2ece9dab73bcb07
fake name <fake@fake.com>
2019-09-20 11:26:28 -0400

    last commit of class

---
+++ py/holamundo.py
@@ -0,0 +1,6 @@
+#!/usr/bin/python3
+
+with open('hellosoftheworld.txt', 'w') as hellos:
+    hellos.write('Ni hao, shijie \n')
+    hellos.write('Hola mundo \n')
+    hellos.write('Kon nichiwa sekai \n')
